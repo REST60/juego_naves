6a4fd1e07451ff35037465b4fed71f16f69ec89d
REST60 <97320175+REST60@users.noreply.github.com>
2023-03-31 11:18:35 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 var ship, ship_img,shipRight_img,shipLeft_img;
+var explosion1,explosion2,explosion3;
 var ufo1,ufo1_img;
 var borderRight,borderLeft;
-var ufoGroup
+var ufoGroup, bulletsGroup;
+var life = 5;
+var score = 0;
+var gameState =  "play";
+var dificultad = 30;
+
 
 function preload(){
     ship_img = loadImage("assets/ship.png");
     shipRight_img = loadImage("assets/ship_right.png");
     shipLeft_img = loadImage("assets/ship_left.png");
     ufo1_img = loadImage("assets/ufo1.png");
+
+    explosion1 = loadSound("sounds/explosion1.mp3");
+    explosion2 = loadSound("sounds/explosion2.mp3");
+    explosion3 = loadSound("sounds/explosion3.mp3");
 }
 
 function setup(){
@@ -26,7 +36,7 @@ function setup(){
     ship.scale = 2;
 
     ufoGroup = new Group();
-
+    bulletsGroup = new Group();
 
 }
 
@@ -35,8 +45,10 @@ function draw(){
     fill("white");  
     text(mouseX + " - " + mouseY, mouseX,mouseY);
 
+    if(gameState === "play"){
+
     if(keyDown(UP_ARROW)){
-        ship.position.y =ship.position.y - 5;
+        ship.position.y =ship.position.y - 9;
         ship.changeAnimation("ship", ship_img);
     }
     if(keyDown(DOWN_ARROW)){
@@ -44,11 +56,11 @@ function draw(){
         ship.changeAnimation("ship", ship_img);
     }
     if(keyDown(RIGHT_ARROW)){
-        changePosition(ship,5,0);
+        changePosition(ship,7,0);
         ship.changeAnimation("shipRight", shipRight_img);
     }
     if(keyDown(LEFT_ARROW)){
-        changePosition(ship,-5,0);
+        changePosition(ship,-7,0);
         ship.changeAnimation("shipLeft", shipLeft_img);
     }
 
@@ -58,19 +70,75 @@ function draw(){
     if(keyWentUp(LEFT_ARROW)){
         ship.changeAnimation("ship", ship_img);
     }
-    if(keyDown("space")){
+    if(keyDown("space") && frameCount%10 === 0){
         var bullets = createSprite(ship.position.x,ship.position.y,10,30);
-        bullets.velocity.y = -8;
+        bullets.velocity.y = -15;
+        bullets.shapeColor=rgb(random(1,255),random(1,255),random(1,255));
         ship.depth = bullets.depth;
         ship.depth = ship.depth+1;
+        bulletsGroup.add(bullets);
     }
 
-    if(ufoGroup.isTouching(borderRight)){
-        ufoGroup.setSpeedAndDirectionEach(1, 100);
+    if(ufoGroup.isTouching(bulletsGroup)){
+        for(var i=0; i < ufoGroup.length; i++){
+            for(var j=0; j < bulletsGroup.length; j++){
+                if(ufoGroup[i].isTouching(bulletsGroup[j])){
+                    ufoGroup[i].destroy();
+                    bulletsGroup[j].destroy();
+                    score = score + 1;
+                    explosion1.play();
+                }
+
+            }
+        }
     }
 
+    if(ufoGroup.isTouching(ship)){
+        for(var i=0; i <ufoGroup.length; i++){
+            if(ufoGroup[i].isTouching(ship)){
+                ufoGroup[i].destroy();
+                life = life - 1;
+            }
+
+        }
+        
+    }
+    
+    /*if(score > 5){
+        dificultad = dificultad - 29;
+        //spawnUfos();
+    }*/
+
+
+    ship.collide(borderLeft);
+    ship.collide(borderRight);
+    ufoGroup.bounceOff(borderRight);
+    ufoGroup.bounceOff(borderLeft);
+
+    spawnUfos();    
+    }
+
+    if(life <= 0){
+        gameState = "end";
+        textSize(70);
+        text("GAME OVER",25,350);
+        textSize(15);
+        text("Presiona ESPACIO para reiniciar",150,400);
+    }
+    if(gameState === "end" && keyDown("space")){
+        gameState = "play";
+        life = 5;
+        score = 0;
+        ship.x = 250;
+        ship.y = 600;
+        //ship.changeAnimation("shipParpadeando",....);
+    }
+    
+    textSize(22);
+    text("VIDAS "+life, 400,50);
+    text("SCORE "+score,390,100);
     drawSprites();
-    spawnUfos();
+
 }
 
 
@@ -80,13 +148,16 @@ function changePosition(sprite,x,y){
 }
 
 function spawnUfos(){
-    if(frameCount%30 === 0){
-    ufo1 =createSprite(random(1,500),0,50,50);
-    ufo1.addImage("ufo1", ufo1_img);
-    ufo1.scale= 3
-    ufo1.velocity.y = random(1,3);
-    ufo1.velocity.x = random(-3,3);
-    ufoGroup.add(ufo1);
-    console.log(ufo1.velocity.y,ufo1.velocity.x);
+    if(frameCount%dificultad === 0){
+        ufo1 =createSprite(random(1,500),0,50,50);
+        ufo1.setCollider("circle",0,0,10);
+        ufo1.debug = false;
+        ufo1.addImage("ufo1", ufo1_img);
+        ufo1.scale= 3
+        ufo1.velocity.y = random(1,5);
+        ufo1.velocity.x = random(-5,5);
+        ufo1.lifetime=800;
+        ufoGroup.add(ufo1);
+        //console.log(ufo1.velocity.y,ufo1.velocity.x);
     }
-}
+}   
