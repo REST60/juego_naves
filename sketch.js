c63125b22717432ef85d73d8b2cb5be03b26804b
REST60 <97320175+REST60@users.noreply.github.com>
2023-04-12 18:55:50 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
-var ship, ship_img,shipRight_img,shipLeft_img;
+var ship, ship_img,shipRight_img,shipLeft_img,ship_damaged;
 var explosion1,explosion2,explosion3;
-var ufo1,ufo1_img;
+var ufo1,ufo1_img,enemy1,enemy1_img;
 var borderRight,borderLeft;
-var ufoGroup, bulletsGroup;
+var ufoGroup, bulletsGroup,enemyBulletsGroup;
 var life = 5;
 var score = 0;
 var gameState =  "play";
 var dificultad = 30;
+var enemyLife = 0;
 
 
 function preload(){
     ship_img = loadImage("assets/ship.png");
     shipRight_img = loadImage("assets/ship_right.png");
     shipLeft_img = loadImage("assets/ship_left.png");
+    ship_damaged = loadImage("assets/ship-damaged.png");
+
     ufo1_img = loadImage("assets/ufo1.png");
+    enemy1_img = loadImage("assets/enemy1.png");
 
     explosion1 = loadSound("sounds/explosion1.mp3");
     explosion2 = loadSound("sounds/explosion2.mp3");
@@ -33,11 +37,14 @@ function setup(){
     ship.addImage("ship",ship_img);
     ship.addImage("shipRight",shipRight_img);
     ship.addImage("shipLeft",shipLeft_img);
+    ship.addImage("shipDamage",ship_damaged);
     ship.scale = 2;
+
+   
 
     ufoGroup = new Group();
     bulletsGroup = new Group();
-
+    enemyBulletsGroup = new Group();
 }
 
 function draw(){
@@ -79,6 +86,8 @@ function draw(){
         bulletsGroup.add(bullets);
     }
 
+
+
     if(ufoGroup.isTouching(bulletsGroup)){
         for(var i=0; i < ufoGroup.length; i++){
             for(var j=0; j < bulletsGroup.length; j++){
@@ -88,7 +97,6 @@ function draw(){
                     score = score + 1;
                     explosion1.play();
                 }
-
             }
         }
     }
@@ -97,6 +105,13 @@ function draw(){
         for(var i=0; i <ufoGroup.length; i++){
             if(ufoGroup[i].isTouching(ship)){
                 ufoGroup[i].destroy();
+                var cuenta = World.seconds;
+                while(cuenta < 3){
+                    console.log(",ak");
+                    ship.changeAnimation("shipDamage",ship_damaged);
+                }
+                
+
                 life = life - 1;
             }
 
@@ -108,7 +123,25 @@ function draw(){
         dificultad = dificultad - 29;
         //spawnUfos();
     }*/
+    if(score === 1){
+        score = 11;
+        enemyLife = 1;
+    
+    }
 
+    if(enemyLife === 1 ){
+        enemy1 = createSprite(250,0);
+        enemy1.addImage("enemy_1",enemy1_img);
+        enemy1.scale = 7;
+        enemy1.velocity.y = 1.5;
+        if(frameCount%10 === 0){
+           var enemyBullets = createSprite(0,0,20,20);
+           enemyBullets.velocity.y = 10;
+           enemyBullets.x = enemy1.x;
+           enemyBullets.y = enemy1.y;
+        }
+    }
+  
 
     ship.collide(borderLeft);
     ship.collide(borderRight);
